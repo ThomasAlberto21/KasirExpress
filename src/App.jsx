3116bd1ed0c3222d31f6b0e916034e92c7915495
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-11 19:45:23 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
+import React from 'react';
 import Sidebar from './components/Sidebar';
 import ProductList from './components/ProductList';
 import Results from './components/Results';
 
-const App = () => {
-  return (
-    <div className='grid grid-cols-1 lg:grid-cols-5 md:grid-cols-3 gap-4'>
-      <div className='col-span-1'>
-        <Sidebar />
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className='grid grid-cols-1 lg:grid-cols-5 md:grid-cols-3 gap-4'>
+        <div className='col-span-1'>
+          <Sidebar />
+        </div>
+        <div className='col-span-3'>
+          <ProductList />
+        </div>
+        <div className='col-span-1'>
+          <Results />
+        </div>
       </div>
-      <div className='col-span-3'>
-        <ProductList />
-      </div>
-      <div className='col-span-1'>
-        <Results />
-      </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
