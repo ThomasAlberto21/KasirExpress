4a783d27210e5fccc334a631a85bb156b9dbf334
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-11 19:51:59 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 import Sidebar from './components/Sidebar';
 import ProductList from './components/ProductList';
 import Results from './components/Results';
+import { API_URL } from './api/api';
+import axios from 'axios';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+    };
+
+    componentDidMount() {
+
+    }
+  }
+
   render() {
     return (
       <div className='grid grid-cols-1 lg:grid-cols-5 md:grid-cols-3 gap-4'>
