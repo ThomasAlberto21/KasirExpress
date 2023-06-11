62578538360de7ee6be6d6b53fefb59b6e452f2d
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-11 19:59:44 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Sidebar from './components/Sidebar';
 import ProductList from './components/ProductList';
 import Results from './components/Results';
-import { API_URL } from './api/api';
 import axios from 'axios';
+import { API_URL } from './api/api';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,22 +12,31 @@ export default class App extends React.Component {
     this.state = {
       menus: [],
     };
+  }
 
-    componentDidMount() {
-
-    }
+  componentDidMount() {
+    axios
+      .get(API_URL + 'products')
+      .then((res) => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
+    console.log(this.state.menus);
     return (
-      <div className='grid grid-cols-1 lg:grid-cols-5 md:grid-cols-3 gap-4'>
-        <div className='col-span-1'>
+      <div className="grid grid-cols-1 lg:grid-cols-5 md:grid-cols-3 gap-4">
+        <div className="col-span-1">
           <Sidebar />
         </div>
-        <div className='col-span-3'>
+        <div className="col-span-3">
           <ProductList />
         </div>
-        <div className='col-span-1'>
+        <div className="col-span-1">
           <Results />
         </div>
       </div>
