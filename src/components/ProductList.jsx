4c9320548a90b74c3fb88979468ab06f54cbe9a2
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-11 20:44:45 +0700

    .

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,3 +1,5 @@
+import { numberWithCommas } from '../utils/numberWithCommas';
+
 /* eslint-disable react/prop-types */
 const ProductList = ({ menu }) => {
   return (
@@ -16,7 +18,7 @@ const ProductList = ({ menu }) => {
           </h5>
         </a>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {menu.harga}
+          Rp. {numberWithCommas(menu.harga)}
         </p>
       </div>
     </div>
