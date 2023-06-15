a739a69ee22346cb2c80f66f643fc6e46f820ddf
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-15 10:05:20 +0700

    .

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import successOrderImg from '../assets/success_order.svg';
 
 export default class Success extends React.Component {
   render() {
-    return <div>Success</div>;
+    return (
+      <div className="flex flex-col justify-center items-center h-screen">
+        <img src={successOrderImg} alt="Sucess Order" className="w-96 mb-5" />
+        <h1 className="font-bold text-4xl text-gray-700">Sukses</h1>
+        <p className="font-normal">TerimaKasih Sudah Memesan</p>
+
+        <Link
+          to="/"
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-6 py-2 mr-2 my-3 "
+        >
+          Kembali
+        </Link>
+      </div>
+    );
   }
 }
