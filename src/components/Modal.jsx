d50bff6041793d9351aa0e5b9719466080ff8cdb
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-16 19:38:12 +0700

    .

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { BsXLg } from 'react-icons/bs';
 
 export const Modal = ({ handleCloseModal }) => {
   return (
@@ -15,31 +16,20 @@ export const Modal = ({ handleCloseModal }) => {
         className="fixed top-0 left-0 right-0 z-50 w-full h-screen flex justify-center items-center bg-black bg-opacity-50"
       >
         <div className="relative w-full max-w-2xl max-h-full">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="relative rounded-lg shadow bg-gray-800">
             <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
                 Terms of Service
               </h3>
               <button
                 type="button"
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                data-modal-hide="defaultModal"
                 onClick={handleCloseModal}
+                data-drawer-target="default-sidebar"
+                data-drawer-toggle="default-sidebar"
+                aria-controls="default-sidebar"
+                className="text-gray-700 bg-white hover:bg-gray-200 hover:text-gray-900 rounded-md text-sm p-3 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
               >
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="sr-only">Close modal</span>
+                <BsXLg />
               </button>
             </div>
 
