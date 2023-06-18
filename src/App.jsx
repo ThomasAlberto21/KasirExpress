f3985e1b8971412e978d16ab68b865c544c2cb12
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-06-18 10:59:50 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
 import Home from './pages/Home';
 import Success from './pages/Success';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/success" element={<Success />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
